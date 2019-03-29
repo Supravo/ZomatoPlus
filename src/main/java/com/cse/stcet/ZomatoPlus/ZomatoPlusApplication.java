c9480c00a7c5dcc758365360b88ffd278a4f8446
Supravo <supravosengupta1@gmail.com>
2019-03-29 16:04:15 +0530

    Initial commit

---
+++ src/main/java/com/cse/stcet/ZomatoPlus/ZomatoPlusApplication.java
@@ -0,0 +1,13 @@
+package com.cse.stcet.ZomatoPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoPlusApplication.class, args);
+	}
+
+}
